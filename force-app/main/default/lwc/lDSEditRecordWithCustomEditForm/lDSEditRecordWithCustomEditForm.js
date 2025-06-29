888635a7752cf7ce90518de42558f2a793d9ef2f
Netravati V Patil <netravatipatil0928@gmail.com>
2025-06-29 22:43:55 +0530

    edit records with custom edit form

--- force-app/main/default/lwc/lDSEditRecordWithCustomEditForm/lDSEditRecordWithCustomEditForm.js
+++ force-app/main/default/lwc/lDSEditRecordWithCustomEditForm/lDSEditRecordWithCustomEditForm.js
@@ -4,4 +4,11 @@ export default class LDSEditRecordWithCustomEditForm extends LightningElement {
 
     @api recordId;
     objectApiName = 'Case';
+
+    /*will use this method when we working on create 
+    handleSuccussSubmit(event)
+    {
+        let formReset = this.template.querySelector('lightning-record-edit-form');
+        formReset.reset();
+    }*/
 }
