5eb5d7ccd2540f5f49c57bcdc39236f753a846e4
Netravati V Patil <netravatipatil0928@gmail.com>
2025-06-30 15:07:37 +0530

    LDS-Create Record using Record-Form

---
+++ force-app/main/default/lwc/lDSCreateARecord/lDSCreateARecord.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class LDSCreateARecord extends LightningElement {
+
+    fields = ['Subject','Status','Type','Priority','Origin','Reason','SLAViolation__c'];
+
+}
